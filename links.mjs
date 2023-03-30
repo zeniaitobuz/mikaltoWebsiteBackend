c7d9192fc2eb489e1267ec5fb8ca0868fff89ff3
zenia banerjee <zenia@itobuz.com>
2023-03-30 15:07:13 -0700

    update

---
+++ links.mjs
@@ -0,0 +1,130 @@
+export const topBanner = "https://i.ibb.co/p2X00Tv/image.jpg";
+
+export const room1 =
+  "https://media-cdn.tripadvisor.com/media/photo-s/1b/f4/64/09/aerial-view.jpg";
+
+export const room2 =
+  "https://www.telegraph.co.uk/content/dam/Travel/hotels/2021/November/ritz-carlton-maldives-p.jpg";
+
+export const room3 =
+  "https://media.cntraveller.com/photos/618010ecd6bb7a9ff97b5406/3:2/w_3000,h_2000,c_limit/15maldives-dec%202021%20issue-Julia%20Friedle.jpg";
+
+export const poolImg =
+  "https://img.staticmb.com/mbcontent//images/uploads/2022/7/swimming-pool.jpg";
+
+export const forestImg =
+  "https://st.hzcdn.com/simgs/pictures/pools/pool-house-exterior-worlds-landscaping-and-design-img~86e138e700f5d0a0_14-8465-1-d2d0e1e.jpg";
+
+export const travelImg1 = "https://i.ibb.co/XFsJfJX/travel-Image1.jpg";
+export const travelImg2 = "https://i.ibb.co/2yD25NW/travel-Image2.jpg";
+export const travelImg3 = "https://i.ibb.co/vY52K35/travel-Image3.jpg";
+
+export const helloImg =
+  "https://www.hdwallpapers.in/download/beach_resort_swimming_pool_hd_beach-1920x1080.jpg";
+
+export const hotelDetails = {
+  url: "https://i.ibb.co/K2C3Hn4/sign.jpg",
+  heading: "The Mikalto Luxury & Comfort Homestay",
+  details:
+    "Welcome to our five-star hotel in Puerto de la Cruz's old town. A marvellous combination of architecture, design and contemporary art. 32 room and 10 uniquely different suites ensure our guests' stay is extra special.",
+};
+export const reviews = {
+  selena: {
+    url: "https://i.ibb.co/hK0DdzL/selena.png",
+    name: "Selena Mayer",
+    date: "2021-02-10",
+    comment: "Wonderful New year trip",
+    review:
+      "One of the best hotels i've ever stayed in. The stuff are very friendly and helpful, they were so helpful it made me feel bad. it includes use of the spa, wounderful, it has a...",
+  },
+  esther: {
+    url: "https://i.ibb.co/TmxG9Bm/esther.png",
+    name: "Esther Howard",
+    date: "2021-02-10",
+    comment: "Recommended",
+    review:
+      "Great resort, professional staffs. It turns out that you're searching for paradise and you have it hear, nearby. Thanks for the warmth, you are fantastic.",
+  },
+  jane: {
+    url: "https://i.ibb.co/pLcgfBv/jane.png",
+    name: "Jane Cooper",
+    date: "2021-02-10",
+    comment: "All good",
+    review:
+      "I love the Resort and its environment. Mikalto is incredibly dedicated, knowledgeable, and helpful. The finished product was beautiful every penny...",
+  },
+};
+
+export const tripImg = "https://i.ibb.co/7YQzHKv/trip.png";
+
+export const chooseHotel = {
+  foodUrl: "https://i.ibb.co/Bckk88w/unique-Cuisine.jpg",
+  yogaUrl: "https://i.ibb.co/ZXDgbf8/yoga.jpg",
+  locUrl: "https://i.ibb.co/mtZRP5f/location.jpg",
+};
+
+export const wellnessService = {
+  url: "https://i.ibb.co/xYCYXrH/wellness.jpg",
+  heading: "WELLNESS",
+  subHeading: "Dedicated to wellness",
+  details:
+    " Aman's approach to wellness is focused on personalisation , privacy and seclusion. Local spa rituals with indigenous ingredients, offer a profoundly therapeutic touch, and a gentle sense of discovery.",
+};
+
+export const giftCardService = {
+  url: "https://i.ibb.co/2NwHZBM/giftCard.jpg",
+  heading: "GIFT CARDS",
+  subHeading: "The gift of future travel",
+  details:
+    "Share extraordinary experiences with the Aman E-Gift Card - delivery instantly and offering memories that last a lifetime",
+};
+
+export const spaService = {
+  url: "https://i.ibb.co/7vjXM6D/spa.jpg",
+  heading: "MIKALTO SHOP",
+  subHeading: "The spa experience from home",
+  details:
+    "Skincare and spa supplements harness powerful natural ingredients to provide balance and optimal welbeing.",
+};
+
+export const adventureService = {
+  url: "https://i.ibb.co/Z8W61M1/adventure.jpg",
+  heading: "EXPERIENCE",
+  subHeading: "Adventure in the wild",
+  details:
+    "Skincare and spa supplements harness powerful natural ingredients to provide balance and optimal welbeing.",
+};
+
+export const showRooms = {
+  standardRoom: {
+    url: "https://hips.hearstapps.com/townandcountry/assets/16/27/1-credit-the-setai-miami-beach.jpg",
+    heading: "STANDARD ROOM",
+    price: "From: $150 / night",
+    details: "Comfortable rooms at comfortable pricings ",
+    bedSize: "Queen Size Bed",
+    capacity: "2 adults, 1 childeren",
+    roomSize: "55 sqm",
+    hotelView: "garden view",
+  },
+  suite: {
+    url: "https://www.galeriemagazine.com/wp-content/uploads/2019/03/243f89e0-8235-11e7-a767-bc310e55dd10_1320x770_154749-1024x597.jpg",
+    heading: "SUITE",
+    price: "From: $300 / night",
+    details:
+      "Comfortable ambience and luxurious interiors with all suite amentities",
+    bedSize: "King Size Bed",
+    capacity: "2 adults, 4 childeren",
+    roomSize: "100 sqm",
+    hotelView: "city view",
+  },
+  presidentSuite: {
+    url: "https://media.architecturaldigest.com/photos/56eae20138cde43e159fc53c/master/pass/most-expensive-suite-burj-al-arab-07.jpg",
+    heading: "PRESIDENT SUITE",
+    price: "From: $400 / night",
+    details: "Live like a president in the suite with everything you need",
+    bedSize: "King Size Bed",
+    capacity: "3 adults, 5 childeren",
+    roomSize: "1500 sqm",
+    hotelView: "garden view",
+  },
+};
---
+++ server.mjs
@@ -0,0 +1,63 @@
+import http from "http";
+import * as fs from "fs/promises";
+import * as links from "./links.mjs";
+
+const port = 200;
+
+function getRequestData(req) {
+  if (req.url === "/") {
+    const status = 200;
+    return JSON.stringify({
+      status,
+      topBanner: links.topBanner,
+      hotelDetails: links.hotelDetails,
+      room1: links.room1,
+      room2: links.room2,
+      room3: links.room3,
+      chooseHotel: links.chooseHotel,
+      poolImg: links.poolImg,
+      forestImg: links.forestImg,
+      wellnessService: links.wellnessService,
+      giftCardService: links.giftCardService,
+      spaService: links.spaService,
+      adventureService: links.adventureService,
+      showRooms: links.showRooms,
+      travelImg1: links.travelImg1,
+      travelImg2: links.travelImg2,
+      travelImg3: links.travelImg3,
+      helloImg: links.helloImg,
+      reviews: links.reviews,
+      tripImg: links.tripImg,
+    });
+  } else {
+    return JSON.stringify("Page not available.");
+  }
+}
+
+const server = http.createServer((req, res) => {
+  try {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    const formDataBase = [];
+    req.on("data", (formDataParts) => {
+      formDataBase.push(formDataParts);
+    });
+    req.on("end", () => {
+      let fullFormdata = Buffer.concat(formDataBase).toString();
+      if (fullFormdata.length > 0) {
+        (async function () {
+          const dataFromFile = await fs.readFile("./js/formData.txt", "utf8");
+          fullFormdata += ";" + dataFromFile;
+          await fs.writeFile("./js/formData.txt", fullFormdata);
+        })();
+      }
+    });
+    res.end(getRequestData(req));
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+server.listen(port, () => {
+  console.log("Server started ... ");
+});
